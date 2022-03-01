eb844da035c5a3c5748b69b7856fea594fec0cef
erwantaupin <taupin.erwan@laposte.net>
2022-03-01 13:12:25 +0100

    finit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action='traitement.php' method="post">
+  <table class="loginTable">
+     <tr>
+      <th>ADMIN PANEL LOGIN</th>
+     </tr>
+     <tr>
+      <td>
+        <label class="firstLabel">Username:</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off" />
+      </td>
+     </tr>
+     <tr>
+      <td><label>Password:</label>
+        <input type="password" name="password" id="password" value="" autocomplete="off" /></td>
+     </tr>
+     <tr>
+      <td>
+         <input type="submit" name="submitBtnLogin" id="submitBtnLogin" value="Login" />
+         <span class="loginMsg"><?php echo @$msg;?></span>
+      </td>
+     </tr>
+  </table>
+</form>
+</body>
+</html>
