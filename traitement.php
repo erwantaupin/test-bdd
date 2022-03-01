c1c44d335b7a447aa99e10ed4240a9705d7f974b
erwantaupin <taupin.erwan@laposte.net>
2022-03-01 13:12:13 +0100

    fonctionnel

---
+++ traitement.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+include("db.php");
+?>
+<?php
+$msg = ""; 
+if(isset($_POST['submitBtnLogin'])) {
+  $username = trim($_POST['username']);
+  $password = trim($_POST['password']);
+  if($username != "" && $password != "") {
+    try {
+      $query = "select * from `user` where `nom_user`=:username and `password_user`=:password";
+      $stmt = $db->prepare($query);
+      $stmt->bindParam('username', $username, PDO::PARAM_STR);
+      $stmt->bindValue('password', $password, PDO::PARAM_STR);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+      $row   = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($count == 1 && !empty($row)) {
+        /******************** Your code ***********************/
+        $_SESSION['sess_user_id']   = $row['id_user'];
+        $_SESSION['sess_user_name'] = $row['nom_user'];
+        // $_SESSION['sess_name'] = $row['nom_user'];
+       header("location: home.php");
+      } else {
+        $msg = "Invalid username and password!";
+      }
+    } catch (PDOException $e) {
+      echo "Error : ".$e->getMessage();
+    }
+  } else {
+    $msg = "Both fields are required!";
+  }
+}
+?>
